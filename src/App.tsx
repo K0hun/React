b464e07877672cdc96071496ed76b9e3dd466c0d
Hun <dudgns9194@gmail.com>
2024-06-19 15:49:41 +0900

    jsx 문법 - 주석 사용방법 + 활용 방법

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+{
+  /*
+  작성자 : 1ch
+  작성일 : 2030.5.30
+  내용 : 기능에 대한 내용
+  */
+}
+
 function App() {
   let name = "리액트";
 
@@ -12,8 +20,7 @@ function App() {
         {name === "리액트" ? <h1>Yes</h1> : null}!!
       </h1>
       <p>반갑습니다.</p>
-      <br/>
-      <input/>
+      {/*주석문 사용하는 방법*/}
     </div>
   );
 }
