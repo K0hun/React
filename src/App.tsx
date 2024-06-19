49ef19624f4508be4fadcb5f4bb52dcdd096f633
Hun <dudgns9194@gmail.com>
2024-06-19 15:46:47 +0900

    jsx 문법 - 셀프클로징(<br/>, <input/>

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,16 @@ import "./App.css";
 
 function App() {
   let name = "리액트";
-  const style = {
-    backgroundColor: "black",
-    color: "white",
-    fontSize: "48px",
-    fontWeight: "bold",
-    padding: "20px"
-  };
 
   return (
-    <div style = {{
-    backgroundColor: "black",
-    color: "white",
-    fontSize: "48px",
-    fontWeight: "bold",
-    padding: "20px"
-  }}>
-      <h1 style = {style}>
+    <div className="container">
+      <h1 className="test">
         Hello,
         {name === "리액트" ? <h1>Yes</h1> : null}!!
       </h1>
       <p>반갑습니다.</p>
+      <br/>
+      <input/>
     </div>
   );
 }
